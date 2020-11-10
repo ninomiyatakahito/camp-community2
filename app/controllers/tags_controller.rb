0c9c74de146edfab991ea7285f578d406b94f217
ninomiyatakahito <ghexcffb@gmail.com>
2020-11-10 16:17:06 +0900

    イベントの表示

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,11 @@
 class TagsController < ApplicationController
   def index
-    @event_tag = EventTag.all
+      @tags = Tag.find(params[:tag_id])
   end
 
+  private
+
+  def event_params
+    params.require(:event).permit(:concept, :place, :period, :image, tag_ids: []).merge(user_id: current_user.id)
+  end
 end
