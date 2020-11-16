5f7ef1ad2a53ee126420cac22cf24d4782b45f50
ninomiyatakahito <ghexcffb@gmail.com>
2020-11-16 17:39:36 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'orders/index'
-  resources :orders
   resources :tags
   devise_for :users
   get 'events/index'
   root to: "events#index"
-  resources :events 
   resources :events do
     resources :orders, only: [:index, :create]
   end
